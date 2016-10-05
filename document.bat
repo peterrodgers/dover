bb3c2d512da89d588faf55500296f7ae2c86524d
Peter Rodgers <P.J.Rodgers@kent.ac.uk>
2016-10-05 19:51:37 +0100

    added command to create javadocs

---
+++ document.bat
@@ -0,0 +1,3 @@
+javadoc -private -use -nodeprecatedlist -doctitle "Graph" -windowtitle "Graph Package" -d doc -sourcepath src uk.ac.kent.dover.fastGraph
+#javadoc -private -use -nodeprecatedlist -doctitle "Graph" -windowtitle "Graph Package" -d doc -sourcepath src pjr.graph pjr.graph.display pjr.graph.comparators pjr.graph.dialogs pjr.graph.drawers pjr.graph.experiments pjr.graph.utilities
+pause
