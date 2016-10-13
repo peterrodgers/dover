9cb3ebef1392d2341485cb63552fb1eb16d57774
Peter Rodgers <P.J.Rodgers@kent.ac.uk>
2016-10-13 10:47:41 +0100

    jar commands

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 "# dover" 
+
+MS Windows only. There will be a similar process for linux and mac, but these are not directly supported.
+Ensure java bin directory is in the path variable
+
+Build the jar by doubleclicking buildjar.bat
+(or run buildjar.bat in the command line)
+Depending on available memory, run the jar from the command line:
+java -jar -Xmx14g dover.jar
+The -Xmx14g sets the java heap to 14 GB and is based on running the program with a machine that has 16GB RAM total. Modify this for your particular configuration
+The interfact window should then appear
+
---
+++ buildjar.bat
@@ -0,0 +1,5 @@
+cd bin
+jar cmf ..\fastGraph_manifest.mf ..\dover.jar uk\ac\kent\dover\fastGraph\*.class uk\ac\kent\displayGraph\*.class uk\ac\kent\displayGraph\comparators\*.class uk\ac\kent\displayGraph\dialogs\*.class uk\ac\kent\displayGraph\display\*.class uk\ac\kent\displayGraph\drawers\*.class uk\ac\kent\displayGraph\experiments\*.class uk\ac\kent\displayGraph\utilities\*.class uk\ac\kent\displayGraph\views\*.class org\json\*.class org\json\LICENSE org\json\README uk\ac\kent\dover\license.txt uk\ac\kent\dover\README.md uk\ac\kent\displayGraph\license.txt uk\ac\kent\displayGraph\README.md 
+
+cd ..
+pause
