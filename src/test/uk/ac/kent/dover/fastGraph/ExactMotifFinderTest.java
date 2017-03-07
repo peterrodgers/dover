536a9ed8512844229e330d400ca9a0acc2b0c198
robbaker292 <robbaker292@gmail.com>
2017-03-07 09:58:44 +0000

    Added JUnit for MotifFinder over time

--- src/test/uk/ac/kent/dover/fastGraph/ExactMotifFinderTest.java
+++ src/test/uk/ac/kent/dover/fastGraph/ExactMotifFinderTest.java
@@ -30,7 +30,7 @@ public class ExactMotifFinderTest {
 		emf.findAllMotifs(0,4,4);
 		
 		ArrayList<MotifResultHolder> results = emf.compareAndExportResults(4, 4, 4);
-		assertEquals(results.get(0).generateDifference(),41.59, 0.1);
+		assertEquals(41.59, results.get(0).generateDifference(), 0.1);
 	}
 	
 	@Test
@@ -43,6 +43,34 @@ public class ExactMotifFinderTest {
 		emf.findAllMotifs(0,4,4);
 		
 		ArrayList<MotifResultHolder> results = emf.compareAndExportResults(4, 4, 4);
-		assertEquals(results.get(0).generateDifference(),54.01, 0.1);
+		assertEquals(54.01, results.get(0).generateDifference(), 0.1);
+	}
+	
+	@Test
+	public void test003() throws IOException {
+		FastGraph g = FastGraph.jsonStringGraphFactory(TestRunner.get4Node5Edge(),false);
+		g = g.randomTimeSeriesFactory(0.2, 0.2, 2, 2, false, false);
+		ExactMotifFinder emf = new ExactMotifFinder(g, new MotifTaskDummy(), false);
+		emf.findAllMotifs(10,4,4);
+
+		emf = new ExactMotifFinder(g, new MotifTaskDummy(), false);
+		emf.findAllMotifs(0,4,4);
+		
+		ArrayList<MotifResultHolder> results = emf.compareAndExportResults(4, 4, 4);
+		assertEquals(17.23, results.get(0).generateDifference(), 0.1);
+	}
+	
+	@Test
+	public void test004() throws IOException {
+		FastGraph g = FastGraph.jsonStringGraphFactory(TestRunner.get5Node5Edge(),false);
+		g = g.randomTimeSeriesFactory(0.2, 0.2, 2, 2, false, false);
+		ExactMotifFinder emf = new ExactMotifFinder(g, new MotifTaskDummy(), false);
+		emf.findAllMotifs(10,4,4);
+
+		emf = new ExactMotifFinder(g, new MotifTaskDummy(), false);
+		emf.findAllMotifs(0,4,4);
+		
+		ArrayList<MotifResultHolder> results = emf.compareAndExportResults(4, 4, 4);
+		assertEquals(15.16, results.get(0).generateDifference(), 0.1);
 	}
 }
